626910078f84d7dee8ee9fa7e787c14f0248ead0
Hector Ubaldo Vera San Roman <a00464819@gmail.com>
2021-01-17 13:14:27 -0700

    Toolbar
    Add toolbar, backbutton and title

---
+++ app/src/main/java/com/mx/baju/activities/BaseActivity.kt
@@ -0,0 +1,57 @@
+package com.mx.baju.activities
+
+import android.app.ActionBar
+import android.app.Dialog
+import android.opengl.Visibility
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.mx.baju.R
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+        setUpToolbar()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> backPressButton()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setUpToolbar() {
+        val toolbar:Toolbar = findViewById(R.id.base_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_arrow_white)
+        showBackNavigationButton(true)
+    }
+
+    fun showBackNavigationButton(show : Boolean) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(show)
+        supportActionBar?.setDisplayShowHomeEnabled(show)
+    }
+
+    fun changeTitle(title:String) {
+        val toolbar:Toolbar = findViewById(R.id.base_toolbar)
+        toolbar.title = title
+    }
+
+    fun changeSubTitle (subtTitle : String) {
+        val toolbar:Toolbar = findViewById(R.id.base_toolbar)
+        toolbar.subtitle = subtTitle
+    }
+
+    fun backPressButton() {
+        Toast.makeText(this, "Back press", Toast.LENGTH_SHORT).show()
+        //onBackPressed()
+    }
+}
