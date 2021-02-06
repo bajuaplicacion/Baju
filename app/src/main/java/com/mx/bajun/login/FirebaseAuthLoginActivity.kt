b8adeb66f746d98d410db7a633ac0f468ca5dcb1
Hector Ubaldo Vera San Roman <a00464819@gmail.com>
2021-02-05 18:50:15 -0700

    Update FirebaseAuthLoginActivity.kt

--- app/src/main/java/com/mx/bajun/login/FirebaseAuthLoginActivity.kt
+++ app/src/main/java/com/mx/bajun/login/FirebaseAuthLoginActivity.kt
@@ -15,10 +15,15 @@ import com.mx.bajun.utils.Constants
 
 class FirebaseAuthLoginActivity : BaseActivity(), View.OnClickListener, View.OnFocusChangeListener {
 
+    private lateinit var etCorreo : EditText
+    private lateinit var btnIngresar : Button
+    private lateinit var tvCrearCuenta : TextView
+    private lateinit var tvErrorMessage : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_auth_login)
-        setUpUI()
+        init()
     }
 
     override fun onClick(p0: View?) {
@@ -34,28 +39,26 @@ class FirebaseAuthLoginActivity : BaseActivity(), View.OnClickListener, View.OnF
         }
     }
 
-    private fun setUpUI() {
+    private fun init() {
         setUpToolbar(true, false)
-        val et_correo : EditText = findViewById(R.id.et_correo)
-        val btn_ingresar : Button = findViewById(R.id.btn_ingresar)
-        val tv_crearCuenta : TextView = findViewById(R.id.tv_crearCuenta)
-        et_correo.onFocusChangeListener = this
-        btn_ingresar.setOnClickListener(this)
-        btn_ingresar.isEnabled = false
-        tv_crearCuenta.setOnClickListener(this)
+        etCorreo = findViewById(R.id.et_correo)
+        btnIngresar  = findViewById(R.id.btn_ingresar)
+        tvCrearCuenta = findViewById(R.id.tv_crearCuenta)
+        tvErrorMessage = findViewById(R.id.tv_errorMessage)
+        etCorreo.onFocusChangeListener = this
+        btnIngresar.setOnClickListener(this)
+        btnIngresar.isEnabled = false
+        tvCrearCuenta.setOnClickListener(this)
     }
 
     private fun checkEmailFormat() {
-        val et_correo : EditText = findViewById(R.id.et_correo)
-        val tv_errorMessage : TextView = findViewById(R.id.tv_errorMessage)
-        val btn_ingresar : Button = findViewById(R.id.btn_ingresar)
-        val sEmail : String = et_correo.text.toString()
+        val sEmail : String = etCorreo.text.toString()
         if (!isValidEmail(sEmail)) {
             errorMessage( getString(R.string.error_email_format) )
         } else {
-            tv_errorMessage.visibility = View.INVISIBLE
-            et_correo.setBackgroundResource(R.drawable.white_background)
-            btn_ingresar.isEnabled = true
+            tvErrorMessage.visibility = View.INVISIBLE
+            etCorreo.setBackgroundResource(R.drawable.white_background)
+            btnIngresar.isEnabled = true
         }
     }
 
@@ -77,18 +80,13 @@ class FirebaseAuthLoginActivity : BaseActivity(), View.OnClickListener, View.OnF
     }
 
     private fun errorMessage(message : String) {
-        val et_correo : EditText = findViewById(R.id.et_correo)
-        val tv_errorMessage : TextView = findViewById(R.id.tv_errorMessage)
-        val btn_ingresar : Button = findViewById(R.id.btn_ingresar)
-        tv_errorMessage.text = message
-        tv_errorMessage.visibility = View.VISIBLE
-        et_correo.setBackgroundResource(R.drawable.error_background)
-        btn_ingresar.isEnabled = false
+        tvErrorMessage.text = message
+        tvErrorMessage.visibility = View.VISIBLE
+        etCorreo.setBackgroundResource(R.drawable.error_background)
+        btnIngresar.isEnabled = false
     }
 
     private fun login() {
 
     }
-
-
 }
