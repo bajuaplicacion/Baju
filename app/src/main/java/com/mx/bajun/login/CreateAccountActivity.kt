aa4b03632261a31c4b4aa0a0f410a150bb72ea87
Hector Ubaldo Vera San Roman <a00464819@gmail.com>
2021-02-05 19:03:38 -0700

    Mensaje de error

--- app/src/main/java/com/mx/bajun/login/CreateAccountActivity.kt
+++ app/src/main/java/com/mx/bajun/login/CreateAccountActivity.kt
@@ -2,12 +2,30 @@ package com.mx.bajun.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.mx.bajun.R
 import com.mx.bajun.base.BaseActivity
 
-class CreateAccountActivity : BaseActivity() {
+class CreateAccountActivity : BaseActivity(), View.OnClickListener, View.OnFocusChangeListener {
+
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        init()
     }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFocusChange(p0: View?, p1: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    private fun init() {
+
+    }
+
 }
